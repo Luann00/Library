90a933e05ea09fcbe9401ae7f695eac8e0347374
Luan Zekiri <luan.00@hotmail.de>
2024-06-28 15:07:31 +0200

    created table for book presentation + inserted delete button for book(only front end, no functionality yet

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const myLibary = [];
+const myLibrary = [];
+
+/*this attribut checks if a first book was created. If yes, the function addBookToLibrary will create a new Table*/
+let firstTime = true;
 
 
 function Book(title, author, pages, read) {
@@ -9,19 +12,84 @@ function Book(title, author, pages, read) {
 }
 
 
-function addBookToLibary() {
-    let newTitle = document.getElementById('title').value
-    let newAuthor = document.getElementById('author').value
-    let newPages = document.getElementById('pages').value
-    let read = document.getElementById('read');
+function addBookToLibrary() {
+    let newTitle = document.getElementById('title').value;
+    let newAuthor = document.getElementById('author').value;
+    let newPages = document.getElementById('pages').value;
+    let read = document.getElementById('read').checked; // use checked property to get boolean value
 
     let newBook = new Book(newTitle, newAuthor, newPages, read);
+    myLibrary.push(newBook);
 
-    myLibary.push(newBook);
-    let newBookIndex = document.createElement("p");
-    newBookIndex.textContent = `Title: ${newTitle}, Author: ${newAuthor}, Pages: ${newPages}, Read: ${read.checked ? "Yes" : "No"}`;
-    document.querySelector(".books").appendChild(newBookIndex);
+    const table = document.querySelector('#pixelCanvas');
+
+    /* if a book is added for the first time, a new table with headers is created which give information about the
+    book such as the title of the book, author ect.*/
+    if (firstTime) {
+        const headerRow = document.createElement('tr');
+
+        const headerTitle = document.createElement('th');
+        headerTitle.textContent = "Title";
+        headerRow.appendChild(headerTitle);
+
+        const headerAuthor = document.createElement('th');
+        headerAuthor.textContent = "Author";
+        headerRow.appendChild(headerAuthor);
+
+        const headerPages = document.createElement('th');
+        headerPages.textContent = "Pages";
+        headerRow.appendChild(headerPages);
+
+        const headerRead = document.createElement('th');
+        headerRead.textContent = "Read";
+        headerRow.appendChild(headerRead);
+
+
+        const deleteButton = document.createElement('th');
+        deleteButton.textContent = "Edit";
+        headerRow.appendChild(deleteButton);
+
+        table.appendChild(headerRow);
+        firstTime = false;
+    }
+
+    const row = document.createElement('tr');
+    table.appendChild(row);
+
+    const cellTitle = document.createElement('td');
+    cellTitle.textContent = newBook.title;
+    cellTitle.style.width = "100px";
+    cellTitle.style.height = "60px";
+
+    const cellAuthor = document.createElement('td');
+    cellAuthor.textContent = newBook.author;
+    cellAuthor.style.width = "100px";
+    cellAuthor.style.height = "60px";
+
+    const cellPages = document.createElement('td');
+    cellPages.textContent = newBook.pages;
+    cellPages.style.width = "100px";
+    cellPages.style.height = "60px";
+
+    const cellRead = document.createElement('td');
+    cellRead.textContent = newBook.read ? "Yes" : "No";
+    cellRead.style.width = "100px";
+    cellRead.style.height = "60px";
+
+    const deleteBookButton = document.createElement('button');
+    deleteBookButton.style.width = "100px";
+    deleteBookButton.style.height = "30px";
+    deleteBookButton.style.alignSelf = "center";
+
+
+    row.appendChild(cellTitle);
+    row.appendChild(cellAuthor);
+    row.appendChild(cellPages);
+    row.appendChild(cellRead);
+    row.appendChild(deleteBookButton);
+
 }
+
 
 
 /*this method gets called after a book was added. all values in the modal gets deleted so at the next time
@@ -43,8 +111,8 @@ showButton.addEventListener("click", () => {
 });
 
 confirmBtn.addEventListener("click", (event) => {
-    event.preventDefault(); 
-    addBookToLibary();
+    event.preventDefault();
+    addBookToLibrary();
     resetValues();
     favDialog.close();
-  });
+});
